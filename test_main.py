b4d3614a4f98b67e992669cedb87614e1641b37a
Kashgary <ab70x9@gmail.com>
2020-05-07 09:57:04 +0300

    Update test_main.py

--- test_main.py
+++ test_main.py
@@ -11,8 +11,6 @@ SECRET = 'TestSecret'
 TOKEN = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJleHAiOjE1NjEzMDY3OTAsIm5iZiI6MTU2MDA5NzE5MCwiZW1haWwiOiJ3b2xmQHRoZWRvb3IuY29tIn0.IpM4VMnqIgOoQeJxUbLT-cRcAjK41jronkVrqRLFmmk'
 EMAIL = 'wolf@thedoor.com'
 PASSWORD = 'huff-puff'
-myPath = os.path.dirname(os.path.abspath(__file__))
-sys.path.insert(0, myPath + '/../')
 
 @pytest.fixture
 def client():
